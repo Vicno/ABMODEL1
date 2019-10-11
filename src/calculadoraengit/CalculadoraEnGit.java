c557c1646582e7d9f6a330184b8f054d1b3eea2c
Vicno <diegorosazza@gmail.com>
2019-10-11 14:18:28 -0400

    menu funcional

--- src/calculadoraengit/CalculadoraEnGit.java
+++ src/calculadoraengit/CalculadoraEnGit.java
@@ -5,6 +5,8 @@
  */
 package calculadoraengit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DIEGO
@@ -16,6 +18,69 @@ public class CalculadoraEnGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        boolean trabajar = true;
+        
+        
+        
+        while(trabajar){
+        System.out.println("elija una opcion");
+        System.out.println("1: Sumar");
+        System.out.println("2: Restar");
+        System.out.println("3: Multiplicar");
+        System.out.println("4: Dividir");
+        System.out.println("0: Salir");
+        int eleccion = scan.nextInt();
+        double v1 = 0;
+        double v2 = 0;
+       
+        switch(eleccion){
+            case 1:
+                System.out.println("Ingrese el primer valor");
+                v1 = scan.nextInt();
+                System.out.println("Ingrese el segundo valor");
+                v2 = scan.nextInt();
+                //suma(v1,v2);
+                
+                break;
+                
+                case 2:
+                System.out.println("Ingrese el primer valor");
+                v1 = scan.nextInt();
+                System.out.println("Ingrese el segundo valor");
+                v2 = scan.nextInt();
+                //resta(v1,v2);
+                
+                break;
+                
+                case 3:
+                System.out.println("Ingrese el primer valor");
+                v1 = scan.nextInt();
+                System.out.println("Ingrese el segundo valor");
+                v2 = scan.nextInt();
+                //multiplicacion(v1,v2);
+                
+                break;
+                
+                case 4:
+                System.out.println("Ingrese el primer valor");
+                v1 = scan.nextInt();
+                System.out.println("Ingrese el segundo valor");
+                v2 = scan.nextInt();
+                //division(v1,v2);
+                
+                break;
+                
+                case 0:
+                trabajar = false;
+                
+                break;
+                
+                
+        }
+        
+        }
+        
     }
     
 }
